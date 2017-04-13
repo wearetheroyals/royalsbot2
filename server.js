a658600ea03b66a47dda27d7d3aa8cf25934f566
Paul Broomfield <paul@theroyals.com.au>
2017-04-13 15:19:06 +1000

    using bot id now

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const Slapp = require('slapp')
 const ConvoStore = require('slapp-convo-beepboop')
 const Context = require('slapp-context-beepboop')
 
-const otherBot = "<!royalsbot1>";
+const otherBot = "<@A4YMUC7JA>";
 
 // use `PORT` env var on Beep Boop - default to 3000 locally
 var port = process.env.PORT || 3000
